fe7f621b5168cd47086ada74e7c87b884a3468f1
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-05 18:18:41 +0100

    Add day-03-1

---
+++ day-03-1/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using Input;
+using Rucksack;
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+
+            Parser parser = new Parser("../input.txt");
+
+            List<RucksackContent> rucksacks = new List<RucksackContent>();
+            foreach((string c1, string c2) content in parser.getRucksackContents()) {
+                rucksacks.Add(new RucksackContent(content.c1, content.c2));
+            }
+
+            int prioritySum = rucksacks.Sum(r => r.getItemPriority(r.getWrongItem()));
+            Console.WriteLine(prioritySum);
+
+        }
+    }
+}
---
+++ day-03-1/src/RucksackContent.cs
@@ -0,0 +1,47 @@
+namespace Rucksack {
+    class RucksackContent {
+        protected char[] compartmentOne { get; set; }
+        protected char[] compartmentTwo { get; set; }
+
+        protected char wrongItem { get; set; }
+
+        public RucksackContent(string c1, string c2) {
+            compartmentOne = fillCompartment(c1);
+            compartmentTwo = fillCompartment(c2);
+
+            wrongItem = findWrongItem();
+        }
+
+        private char[] fillCompartment(string contents) {
+            
+            int index = 0;
+            char[] comp = new char[contents.Length];
+            foreach (char s in contents.OrderBy(c => c)) {
+                comp[index] = s;
+                index += 1;
+            }
+
+            return comp;
+        }
+
+        private char findWrongItem() {
+            for (int i = 0; i < compartmentOne.Length; i++) {
+                for (int j = 0; j < compartmentTwo.Length; j++ ) {
+                    if (compartmentOne[i] == compartmentTwo[j]) {
+                        return compartmentTwo[j];
+                    }
+                }
+            }
+            return new char();
+        }
+
+        public int getItemPriority(char item) {
+            return (int)Enum.Parse(typeof(Priority), item.ToString());
+        }
+
+        public char getWrongItem() {
+            return wrongItem;
+        }
+
+    }
+}
